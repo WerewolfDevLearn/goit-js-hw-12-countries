722a64283ffb9b03a2c7fb1a77528a288ed6d3d2
Mikhail <werewolfdevlearn@gmail.com>
2020-08-08 20:32:47 +0300

    update markup-creation

--- src/js/markups-creation.js
+++ src/js/markups-creation.js
@@ -4,11 +4,11 @@ import { baseInfo } from './notification.js';
 
 function createMarkup(arrOfCountries) {
   let markup;
-
-  if (arrOfCountries.length > 10) {
+  if (!arrOfCountries) {
+    return;
+  } else if (arrOfCountries.length > 10) {
     baseInfo();
-  } else if (arrOfCountries.length <= 10 && arrOfCountries.length > 2) {
-    baseInfo();
+  } else if (arrOfCountries.length <= 10) {
     markup = countriesTpl(arrOfCountries);
     return markup;
   } else if (arrOfCountries.length < 2) {
